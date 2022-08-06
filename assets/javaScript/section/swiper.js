fccb886b7d52350d9225a96ab14117056e2ab9a0
HossamEisa <39234961+HossamEisa@users.noreply.github.com>
2022-08-06 16:44:17 +0200

    fix carosuels issues and header

--- assets/javaScript/section/swiper.js
+++ assets/javaScript/section/swiper.js
@@ -1,6 +1,6 @@
 // Swiper Js
 
-export let swiperHome = new Swiper( "#home .mySwiperHome", {
+export let swiperHome = new Swiper("#home .mySwiperHome", {
 
      navigation: {
           nextEl: ".swiper-button.next",
@@ -10,20 +10,20 @@ export let swiperHome = new Swiper( "#home .mySwiperHome", {
      //      delay: 10000,
      //      disableOnInteraction: false,
      // },
-} );
+});
 
-export let swiperServices = new Swiper( ".mySwiperServices", {
+export let swiperServices = new Swiper(".mySwiperServices", {
      spaceBetween: 50,
      navigation: {
           nextEl: "#services .swiper-button.next",
           prevEl: "#services .swiper-button.prev",
      },
-} );
+});
 
 
 
 
-export let swiperMEDIU = new Swiper( ".mySwiperMEDIU", {
+export let swiperMEDIU = new Swiper(".mySwiperMEDIU", {
      slidesPerView: "auto",
      spaceBetween: 15,
      navigation: {
@@ -45,21 +45,21 @@ export let swiperMEDIU = new Swiper( ".mySwiperMEDIU", {
                spaceBetween: 15,
           },
      },
-} );
-export let swiperMediuTab3 = new Swiper( ".mySwiperMEDIU.tab-3", {
+});
+export let swiperMediuTab3 = new Swiper(".mySwiperMEDIU.tab-3", {
      spaceBetween: 10,
      slidesPerView: 4,
      freeMode: true,
      watchSlidesProgress: true,
-} );
-export let swiperMediuTab32 = new Swiper( ".mySwiperMEDIU-2.tab-3", {
+});
+export let swiperMediuTab32 = new Swiper(".mySwiperMEDIU-2.tab-3", {
      spaceBetween: 0,
      thumbs: {
           swiper: swiperMediuTab3,
      },
-} );
+});
 
-export let swiperMediuTab4 = new Swiper( ".mySwiperMediuTab4", {
+export let swiperMediuTab4 = new Swiper(".mySwiperMediuTab4", {
      spaceBetween: 30,
      slidesPerGroup: 2,
      autoplay: {
@@ -87,9 +87,9 @@ export let swiperMediuTab4 = new Swiper( ".mySwiperMediuTab4", {
 
 
      },
-} );
+});
 
-export let swiperAcademics = new Swiper( ".mySwiperAcademics", {
+export let swiperAcademics = new Swiper(".mySwiperAcademics", {
      spaceBetween: 30,
      slidesPerGroup: 2,
      autoplay: {
@@ -128,14 +128,14 @@ export let swiperAcademics = new Swiper( ".mySwiperAcademics", {
 
 
      },
-} );
+});
 
 
 
 
 
 // swiper in home
-export let mySwiperHome3 = new Swiper( ".mySwiperHome3", {
+export let mySwiperHome3 = new Swiper(".mySwiperHome3", {
      spaceBetween: 0,
      slidesPerGroup: 1,
      autoplay: {
@@ -158,12 +158,12 @@ export let mySwiperHome3 = new Swiper( ".mySwiperHome3", {
 
 
      },
-} );
+});
 
 
 
 // Swiper In Apply
-export let swiperApplay = new Swiper( ".mySwiperApplay", {
+export let swiperApplay = new Swiper(".mySwiperApplay", {
      spaceBetween: 30,
      pagination: {
           el: ".swiper-pagination",
@@ -173,102 +173,63 @@ export let swiperApplay = new Swiper( ".mySwiperApplay", {
           delay: 7000,
           disableOnInteraction: false,
      },
-} );
+});
 
 
 // swiper in journals in mediu
-export let swiperJournals = new Swiper( ".swiperJournals", {
-     spaceBetween: 0,
-     slidesPerGroup: 1,
+export let swiperJournals = new Swiper(".swiperJournals", {
+     slidesPerView: 1,
      autoplay: {
           delay: 8000,
-          disableOnInteraction: false,
      },
      navigation: {
-          nextEl: "#academics .swiper-button.next",
-          prevEl: "#academics .swiper-button.prev"
+          nextEl: "#mediu-center .Journals .swiper-button.next",
+          prevEl: "#mediu-center .Journals .swiper-button.prev",
      },
      breakpoints: {
-          1665: {
-               slidesPerView: 3.2,
-               spaceBetween: 15,
-          },
-          1114: {
-               slidesPerView: 3,
-               spaceBetween: 10,
-          },
-          780: {
-               slidesPerView: 2.5,
-               spaceBetween: 10,
-          },
-          400: {
-               slidesPerView: 1.5,
-               spaceBetween: 10,
-          },
-          340: {
-               slidesPerView: 1,
-               spaceBetween: 10,
-          },
-
-
-     },
-} );
-
-// swiper in discussions in mediu
-export let swiperDiscussions = new Swiper( ".swiper-discussions", {
-     spaceBetween: 30,
-     slidesPerGroup: 2,
-     autoplay: {
-          delay: 8000,
-          disableOnInteraction: false,
-     },
-     navigation: {
-          nextEl: "#academics .swiper-button.next",
-          prevEl: "#academics .swiper-button.prev"
-     },
-     breakpoints: {
-          1114: {
-               slidesPerView: 3,
-               spaceBetween: 15,
-          },
-          1024: {
-               slidesPerView: 2.9,
-               spaceBetween: 15,
-          },
           992: {
-               slidesPerView: 2.5,
+               slidesPerView: 3,
                spaceBetween: 15,
           },
-          780: {
+          720: {
                slidesPerView: 2,
                spaceBetween: 15,
           },
-          400: {
-               slidesPerView: 1.5,
-               spaceBetween: 15,
-          },
-          340: {
-               slidesPerView: 1,
-               spaceBetween: 15,
-          },
-
-
      },
-} );
+});
+
+// swiper in discussions in mediu
+export let swiperDiscussions = new Swiper(".swiper-discussions", {
+     slidesPerView: 1,
+     autoplay: {
+          delay: 8000,
+     },
+     navigation: {
+          nextEl: "#mediu-center .Discussions .swiper-button.next",
+          prevEl: "#mediu-center .Discussions .swiper-button.prev",
+     },
+     breakpoints: {
+          992: {
+               slidesPerView: 3,
+               spaceBetween: 15,
+          },
+          720: {
+               slidesPerView: 2,
+               spaceBetween: 15,
+          },
+     },
+});
 
 
-const btnAutoPlay = document.querySelector( '.swiper-button.auto' )
+const btnAutoPlay = document.querySelector('.swiper-button.auto')
 
 
 
 
-export let autoPlay = btnAutoPlay.addEventListener( 'click', ( e ) => {
-     if ( swiperServices.autoplay.running == false ) {
+export let autoPlay = btnAutoPlay.addEventListener('click', (e) => {
+     if (swiperServices.autoplay.running == false) {
           swiperServices.autoplay.start()
      } else {
           swiperServices.autoplay.stop()
      }
-} )
-
-
-
+})
