87b68b5de41fb0daf5925d4db243fd208a5e02e6
HossamEisa <39234961+HossamEisa@users.noreply.github.com>
2022-08-13 17:17:06 +0200

    change stracture

--- assets/javaScript/jquery.theme-script.js
+++ assets/javaScript/jquery.theme-script.js
@@ -153,3 +153,24 @@ jQuery(document).ready(function () {
 		}, i.init()
 	}
 }(jQuery);
+
+
+
+jQuery(document).ready(function () {
+	"use strict";
+
+	/* Responsive Navigation Function Start */
+	(function ($) {
+		$('#navigation').cmsmastersResponsiveNav();
+	})(jQuery);
+
+
+});
+
+
+
+/* CMSMasters Media Width */
+function cmsmasters_media_width() {
+	var media_width = parseInt(jQuery('.cmsmasters_responsive_width').css('width'));
+	return media_width;
+}
